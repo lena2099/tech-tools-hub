5bae5e35cae161033c9d3091a3c6c5a33ac61411
lena2099 <wenyu1001@qq.com>
2026-08-03 22:10:51 +0800

    fix: remove unused xmlns:news namespace from sitemap generator

--- .github/scripts/seo_engine.py
+++ .github/scripts/seo_engine.py
@@ -372,7 +372,7 @@ def generate_enhanced_sitemap():
 
     sitemap = f"""<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+        >
 {chr(10).join(urls)}
 </urlset>
 """
